d5fe7dbc855132b41560ef6854523f5daebbdb78
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2023-03-10 11:59:01 +0000

    Created an eventListener to the button and created the function to convert the values from the input in the page

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let input = document.getElementById("darkmode-toggle")
-let containers = document.querySelectorAll(".container")
+let input = document.getElementById("darkmode-toggle");
+let containers = document.querySelectorAll(".container");
+let btn = document.getElementById("convert");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
+let volume = document.getElementById("volume");
+let mass = document.getElementById("mass");
+
+
 
 input.addEventListener('change', function() {
 if(this.checked) {
@@ -10,3 +17,28 @@ document.body.classList.remove('dark-mode');
 containers.forEach(container => container.classList.remove('dark-mode'));
 }
 });
+
+btn.addEventListener('click', function() {
+    const conversionMF = number.value * 3.28084;
+    const conversionFM = number.value * 0.304;
+    const conversionLG = number.value * 0.264172;
+    const conversionGL = number.value * 0.785411784;
+    const conversionKP = number.value * 2.2;
+    const conversiomPK = number.value * 0.45359237;
+    
+    if(number != null) {
+        length.textContent = `${number.value} meters = ${conversionMF} feet || ${number.value} feet = ${conversionFM} meters`;
+        volume.textContent =  `${number.value} liters = ${conversionLG} gallons || ${number.value} gallons = ${conversionGL} liters`;
+        mass.textContent = `${number.value} kilograms = ${conversionKP} pounds || ${number.value} pounds = ${conversiomPK} kilograms`;
+    }
+})
+
+
+// Conversion formulas
+// from meters to feet: n*3.28084
+//reverse: n*0.304
+// from leters to gallons: n*0.264172 
+//reverse: n*0.785411784
+// from kilograms to pounds: n*2.2
+//reverse: n*0.45359237
+
